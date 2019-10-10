384e8d4f3a5ee20cd9c78bfcc6eeea881fb363da
Natalie Laughlin <nalaughlin2@ksu.edu>
2019-10-09 19:14:22 -0500

    Most done

---
+++ PointOfSale/ComboSelection.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale
+{
+    /// <summary>
+    /// Interaction logic for ComboSelection.xaml
+    /// </summary>
+    public partial class ComboSelection : PageFunction<String>
+    {
+        public ComboSelection()
+        {
+            InitializeComponent();
+        }
+
+        private void Brontowurst(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddToCombo());
+        }
+        private void DinoNuggets(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddToCombo());
+        }
+        private void PBJ(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddToCombo());
+        }
+        private void Wings(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddToCombo());
+        }
+        private void TRex(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddToCombo());
+        }
+        private void Wrap(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddToCombo());
+        }
+        private void Burger(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddToCombo());
+        }
+    }
+}
---
+++ PointOfSale/MenuCategorySelection.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale
+{
+    /// <summary>
+    /// Interaction logic for Page11.xaml
+    /// </summary>
+    public partial class MenuCategorySelection : Page
+    {
+        public MenuCategorySelection()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectDrink(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new DrinkSelection());
+        }
+        private void SelectCombo(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ComboSelection());
+        }
+        private void SelectSides(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new SideSelection());
+        }
+        private void SelectEntree(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new SelectEntree());
+        }
+        /*
+         *    <WrapPanel.Resources>
+            <Style TargetType="Button">
+                <Setter Property="MinWidth" Value="140"/>
+
+                <Setter Property="MinHeight" Value="140"/>
+            </Style>
+        </WrapPanel.Resources>*/
+    }
+}
